727cbcddc709f3042c9c4d158e03004b98cbb3ed
Bryce Ashey <bashey@alifemedical.com>
2015-08-14 10:54:06 -0700

    added test projects.

---
+++ Disty.Common.IOC.Test.Unit/ContainerInstanceProviderTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using Autofac;
+using log4net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Disty.Common.IOC.Test.Unit
+{
+    [TestClass]
+    public class ContainerInstanceProviderTest
+    {
+        [TestMethod]
+        public void CanGetAContainerWithoutAssembly()
+        {
+            var container = ContainerInstanceProvider.GetContainerInstance();
+            Assert.IsNotNull(container);
+        }
+
+        [TestMethod]
+        public void CanGetAContainerWithAssembly()
+        {
+            var container = ContainerInstanceProvider.GetContainerInstance(Assembly.GetCallingAssembly());
+            Assert.IsNotNull(container);
+            Assert.IsTrue(container is IContainer);
+        }
+
+        [TestMethod]
+        public void CanActuallyResolveFromContainer()
+        {
+            var container = ContainerInstanceProvider.GetContainerInstance(Assembly.GetCallingAssembly());
+            var testObj = container.Resolve<ILog>();
+            Assert.IsNotNull(testObj);
+            Assert.IsTrue(testObj is ILog);
+        }
+    }
+}
